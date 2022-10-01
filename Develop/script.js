9ba4b8ef48534819672edd2362e0fbd907408245
Megan Rakow <meganrakow@Megans-MBP-2.lan>
2022-09-30 20:53:27 -0500

    edits to html and JS

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,51 @@
 
-// WHEN I view the time blocks for that day
-// THEN each time block is color-coded to indicate whether it is in the past, present, or future
 // WHEN I click the save button for that time block
 // THEN the text for that event is saved in local storage
 // WHEN I refresh the page
 // THEN the saved events persist
 var timeDisplay = $('#currentDay');
-var btnEl = $('#btn');
-var textEl = $('#text');
+var btnEl = $('button');
+var textEl = $('textarea');
 
 
 var day = moment().format('dddd, MMMM Do');
 timeDisplay.text(day);
 
 
+textEl.each(function(){
+    var rowTime = $(this).attr('id');
+    console.log(rowTime)
+    var currentTime = moment().hour();
+    console.log(currentTime)
+    
+    if (rowTime < currentTime){
+        $(this).addClass('past');
+    }else if (rowTime > currentTime){
+        $(this).addClass('future')
+    }else{
+         $(this).addClass('present')
+       
+    }
+})
+
     btnEl.on("click",function(){
-          localStorage.setItem("input", textEl.val);
-        var value = (textEl.val(localStorage.getItem('input')));
+        var value = ($(this).parent().siblings('textarea').val())
+        var idEl = ($(this).parent().siblings('textarea').attr('id'))
+        localStorage.setItem(idEl, value)
     });
 
+    function getTime(){
+        var input = localStorage.getItem('9')
+        $('#9').text(input)
+        $('#10').text(localStorage.getItem('10'))
+        $('#11').text(localStorage.getItem('11'))
+        $('#12').text(localStorage.getItem('12'))
+        $('#1').text(localStorage.getItem('1'))
+        $('#2').text(localStorage.getItem('2'))
+        $('#3').text(localStorage.getItem('3'))
+        $('#4').text(localStorage.getItem('4'))
+        $('#5').text(localStorage.getItem('5'))
+    }
+getTime()
 
 
